53055bbf8695b9b509faeb8dffa9eed8f7a75f31
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-10 17:29:43 +0100

    create search params

---
+++ src/searchParams.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const searchParam = () => {
+    const location = "San Fransisco";
+
+
+    return (
+        <div className="search-params">
+
+        </div>
+    )
+}
