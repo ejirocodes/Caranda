9bf15002168a0ce5f76b39b33177e40e5dd930f3
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-14 13:05:30 +0100

    create custom hooks

--- src/useDropdown.jsx
+++ src/useDropdown.jsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
-
-
+import React, { Component } from "react";
 
 const useDropdown = (label, defaultState, options) => {
-
-}
-
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={e => setState.target.value}
+        onChange={e => setState.target.value}
+        disabled={options.length === 0}
+      >
+        <option value="All">All</option>
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [state, Dropdown, setState];
+};
 
 export default useDropdown;
