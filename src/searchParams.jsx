5a9a743c895a915222b75dbfb41915bbb52bcdf5
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-15 16:58:29 +0100

    add breed dropdown feature

--- src/searchParams.jsx
+++ src/searchParams.jsx
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
-
 const SearchParams = () => {
-  const [location, updateLocation] = useState('San Franscisco, CA');
+  const [location, updateLocation] = useState("San Franscisco, CA");
   const [breeds, updateBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropdown, updateBreed] = useDropdown('Breed', "", breeds);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal, updateBreed, updateBreeds]);
 
   return (
     <div className="search-params">
