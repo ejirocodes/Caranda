cb8de89e3c7732724bc2598688d5ebde343eb338
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-21 10:07:01 +0100

    apply theme context to app and details button

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./errorBoundary";
+import ThemeContext from './ThemeContext'
 
-class Details extends React.Component {
+class Details extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -40,7 +41,11 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+            <button style={{backgroundColor: theme}} onClick={this.toggleModal}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
--- src/searchParams.jsx
+++ src/searchParams.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 import Results from './Results'
+import ThemeContext from './ThemeContext'
 
 const SearchParams = () => {
+  const [theme] = useContext(ThemeContext)
   const [location, updateLocation] = useState("San Franscisco, CA");
   const [breeds, updateBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
@@ -49,7 +51,7 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <button style={{backgroundColor: theme}}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
